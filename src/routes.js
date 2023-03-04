b22002414d76934d970c3bdcbfc12fcf1d2556ba
arindra97 <ar28297@gmail.com>
2023-03-04 10:36:13 +0700

    feat: web service notes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,36 @@
-const {addNoteHandler} = require('./handler');
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNotesByIdHandler,
+  editNoteByIdHandler,
+  deteleNoteByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNotesByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deteleNoteByIdHandler,
   },
 ];
 
