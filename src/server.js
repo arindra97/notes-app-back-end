eac5e4614b326b784702b74707b51d56958de1f3
arindra97 <ar28297@gmail.com>
2023-03-06 20:06:16 +0700

    feat: automation test postman

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
+    port: process.env.NODE_ENV !== 'production' ? 8000 : 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
